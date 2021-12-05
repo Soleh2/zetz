6e16a2f9bb08b89ea345a061b505cfacd26341d1
Soleh2 <90469140+Soleh2@users.noreply.github.com>
2021-12-05 16:21:11 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -3,17 +3,17 @@ let handler = async (m, { conn }) => {
     pepe = await conn.getProfilePicture('62831287340122@s.whatsapp.net').catch(() => 'https://i.ibb.co/jr9Nh6Q/Thumb.jpg')
     baper = await fetch(pepe).then(a => a.buffer())
     let listMessage = {
-        "title": "Melcanz-Bot",
+        "title": "Zets Bot",
         "description": `
-Melcanz PRICE
+Zets PRICE
         
 1 Grup / 30 Hari
-Rp. 15,000 Dana, Ovo
-Rp. 20,000 Pulsa Axis/XL
+Rp. 15,000 Dana, Gopay
+Rp. 20,000 Pulsa Telkom
 
-1 Premium / sampai pensi
-Rp. 20,000 Dana, Ovo
-Rp. 25,000 Pulsa Axis/XL
+1 Premium / Sampai pensi
+Rp. 20,000 Dana, Gopay
+Rp. 25,000 Pulsa Telkom
 `.trim(),
         "listType": "PRODUCT_LIST",
         "productListInfo": {
@@ -22,18 +22,18 @@ Rp. 25,000 Pulsa Axis/XL
                     "title": "Klik untuk melihat harga",
                     "products": [
                         {
-                            "productId": "4696956640315324"
+                            "productId": "5156160351068038"
                         }
                     ]
                 }
             ],
             "headerImage": {
-                "productId": "4696956640315324",
+                "productId": "5156160351068038",
                 "jpegThumbnail": baper
             },
-            "businessOwnerJid": "6283128734012@s.whatsapp.net"
+            "businessOwnerJid": "18554394933@s.whatsapp.net"
         },
-        "footerText": "hubungi wa.me/6283128734012 atau wa.me/6287755080455"
+        "footerText": "hubungi wa.me/6281252848955"
     }
 
 
@@ -41,6 +41,6 @@ Rp. 25,000 Pulsa Axis/XL
 }
 handler.help = ['sewabot']
 handler.tags = ['main']
-handler.command = /^sewa(bot|melcanz)$/i
+handler.command = /^sewa(bot|zets)$/i
 
 module.exports = handler
